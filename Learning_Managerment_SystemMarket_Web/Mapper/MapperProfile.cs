66f35b7a4a3f826750ca6e222429a0f1cb82c373
chien1108 <chiendao2000@gmail.com>
2021-11-17 21:59:31 +0700

    Initial By ChienDT9

---
+++ Learning_Managerment_SystemMarket_Web/Mapper/MapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace Learning_Managerment_SystemMarket_Web.Mapper
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+
+        }
+    }
+}
---
+++ Learning_Managerment_SystemMarket_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Learning_Managerment_SystemMarket_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
