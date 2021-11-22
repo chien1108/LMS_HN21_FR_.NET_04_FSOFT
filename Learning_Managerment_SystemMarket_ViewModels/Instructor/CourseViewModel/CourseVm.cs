66c38d3c8819e2bfa1c543c25b847eac4da3a5c4
SonNguyen <nguyenlamson1609@gmail.com>
2021-11-22 15:03:19 +0700

    Create Course View Model SonNL4

---
+++ Learning_Managerment_SystemMarket_ViewModels/Instructor/CourseViewModel/CourseVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Managerment_SystemMarket_ViewModels.Instructor.CourseViewModel
+{
+    class CourseVm
+    {
+    }
+}
