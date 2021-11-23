a4daa4a91d42eab8319df2c4e87dad308a414faa
duongtung99 <duongphitung12a5@gmail.com>
2021-11-23 14:28:03 +0700

    Add Studnet Controller

--- Learning_Managerment_SystemMarket_Core/Models/Entities/Student.cs
+++ Learning_Managerment_SystemMarket_Core/Models/Entities/Student.cs
@@ -8,7 +8,6 @@ namespace Learning_Managerment_SystemMarket_Core.Models.Entities
     {
         public string StudentName { get; set; }
         public StatusStudent Status { get; set; }
-
         public ICollection<Cart> Carts { get; set; }
         public ICollection<Notification> Notifications { get; set; }
         //public ICollection<ChatGroup> ChatGroups { get; set; }
