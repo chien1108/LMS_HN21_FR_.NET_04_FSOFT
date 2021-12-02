6a3e905dd56a43915f314592b24483e8e5880e42
PTG <thaibang2620@gmail.com>
2021-12-02 17:19:49 +0700

    Student Subcribe Instructor BangNT8

--- Learning_Managerment_SystemMarket_Core/Repositories/StudentRepo/StudentRepository.cs
+++ Learning_Managerment_SystemMarket_Core/Repositories/StudentRepo/StudentRepository.cs
@@ -19,7 +19,12 @@ namespace Learning_Managerment_SystemMarket_Core.Repositories.StudentRepo
 
         public async Task CreateSubcription(SubScription subScription)
         {
-            await _context.AddAsync(subScription);
+            await _context.SubScriptions.AddAsync(subScription);
+        }
+
+        public async Task DeleteSubcription(SubScription subScription)
+        {
+             _context.SubScriptions.Remove(subScription);
         }
 
         public async Task<ICollection<SubScription>> GetAllSubInstructorByStudentId(int studentId)
@@ -29,6 +34,12 @@ namespace Learning_Managerment_SystemMarket_Core.Repositories.StudentRepo
                 .Where(x => x.StudentId == studentId)
                 .ToListAsync();
             return subInstructor;
+        }
+
+        public async Task<bool> IsSubcribleExist(int studentId, int instuctorId)
+        {
+            var result = await _context.SubScriptions.FirstOrDefaultAsync(x=>x.StudentId == studentId && x.InstructorId == instuctorId);
+            return result != null;
         }
     }
 }
