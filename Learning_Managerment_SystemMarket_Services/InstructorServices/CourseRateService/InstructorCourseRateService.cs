ee449fdd95aaa7ff7cb6e9d2dee71692239f8bad
SonNguyen <nguyenlamson1609@gmail.com>
2021-12-03 19:58:46 +0700

    Create CourseRate SonNL4

---
+++ Learning_Managerment_SystemMarket_Services/InstructorServices/CourseRateService/InstructorCourseRateService.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Learning_Managerment_SystemMarket_Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Managerment_SystemMarket_Services.InstructorServices.CourseRateService
+{
+    public class InstructorCourseRateService : IInstructorCourseRateService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public InstructorCourseRateService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+    }
+}
