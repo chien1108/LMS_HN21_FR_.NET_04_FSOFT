e4e8eb5cecb8b7da6aa7a4fb623a11409008bb80
SonNguyen <nguyenlamson1609@gmail.com>
2021-12-04 14:15:42 +0700

    updare Review Course of Instructor SonNL4

---
+++ Learning_Managerment_SystemMarket_ViewModels/Instructor/StudentViewModel/StudentVm.cs
@@ -0,0 +1,13 @@
+﻿using Learning_Managerment_SystemMarket_Core.Modules.Enums;
+
+namespace Learning_Managerment_SystemMarket_ViewModels.Instructor.StudentViewModel
+{
+    public class StudentVm
+    {
+        public string StudentName { get; set; }
+
+        public StatusStudent Status { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
